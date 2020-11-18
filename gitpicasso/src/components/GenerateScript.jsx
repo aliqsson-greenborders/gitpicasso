b532f401957b4987bdaa01960fffb1960e678853
Aziz <abbdullaev@Azizs-MBP.attlocal.net>
2020-11-17 18:32:55 -0600

    deleted unnecessary folder

--- gitpicasso/src/components/GenerateScript.jsx
+++ gitpicasso/src/components/GenerateScript.jsx
@@ -38,7 +38,8 @@ function handleSubmit(commits, repo, github) {
     // scriptLines.push('git branch -M main\n')
     scriptLines.push(`git remote add origin git@github.com:${github}/${repo}.git\n`)
     // scriptLines.push('git branch -M main\n')
-    // scriptLines.push(`git pull origin main \n`)
+    scriptLines.push('git fetch\n')
+    scriptLines.push(`git pull origin master \n`)
     scriptLines.push('git push -u origin master\n')
 
 
